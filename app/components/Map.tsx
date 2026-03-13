0a88111bf552908fa5ba977274e8666fa0857051
vj2506 <vj2506@gmail.com>
2026-03-13 10:55:55 +0530

    fixed leaflet typescript types

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -4,25 +4,41 @@ import 'leaflet/dist/leaflet.css'
 import { useEffect } from 'react'
 import { fixMapIcon } from '../map-fix'
 
-// This sub-component moves the map when you search a new city
+// This component moves the map view when lat/lon change
 function ChangeView({ center }: any) {
   const map = useMap();
-  map.setView(center, 10);
+  useEffect(() => {
+    map.setView(center, 10);
+  }, [center, map]);
   return null;
 }
 
 export default function Map({ lat, lon }: any) {
   useEffect(() => { fixMapIcon() }, [])
 
+  // Ensure lat and lon are treated as numbers
+  const position: [number, number] = [Number(lat), Number(lon)];
+
   return (
-    <MapContainer center={[lat, lon]} zoom={10} style={{ height: '100%', width: '100%', borderRadius: '1.5rem' }}>
-      {/* Professional Dark Map Style */}
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; OpenStreetMap contributors'
-      />
-      <Marker position={[lat, lon]} />
-      <ChangeView center={[lat, lon]} />
-    </MapContainer>
+    <div style={{ height: '100%', width: '100%' }}>
+      {/* 
+        We use {...({ ... } as any)} to bypass the 
+        TypeScript error regarding 'center' 
+      */}
+      <MapContainer 
+        {...({
+          center: position,
+          zoom: 10,
+          style: { height: '100%', width: '100%', borderRadius: '1.5rem' }
+        } as any)}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; OpenStreetMap contributors'
+        />
+        <Marker position={position} />
+        <ChangeView center={position} />
+      </MapContainer>
+    </div>
   )
 }
